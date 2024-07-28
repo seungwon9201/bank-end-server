c3c75684140f2e1983f02b12af0bbcffe510b085
seungwon9201 <ey8968@naver.com>
2024-07-28 17:12:19 +0900

    수정한 데이터 DB에 갱신

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -65,4 +65,20 @@ public class ArticleController {
         //뷰 페이지 설정하기
         return "articles/edit";
     }
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+        log.info(form.toString());
+        //1. DTO를 엔티티로 변환하기
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+        //2. 엔티티를 DB에 저장하기
+        //2-1. 엔티티를 DB에 저장하기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        //2-2. 기존 데이터 값을 갱신하기
+        if (target != null) {
+            articleRepository.save(articleEntity);
+        }
+        //3. 수정 결과 페이지로 리다이렉트하기
+        return"redirect:/articles/" + articleEntity.getId();
+    }
 }
